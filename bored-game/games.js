072d21abbf889e60315505e1d5dcade5ab88140d
jmternes <68615887+jmternes@users.noreply.github.com>
2023-11-15 15:51:10 -0600

    JS rework

--- bored-game/games.js
+++ bored-game/games.js
@@ -3,7 +3,7 @@
 gamesData.forEach(game => {
     console.log(game.GameID);
   });
-  
+
 
 gamesData.forEach(game => {
     console.log(game.Name);
@@ -17,9 +17,24 @@ function displayGameDetails(gameId) {
         document.getElementById('gameDescription').textContent = game.description || 'No description available';
         document.getElementById('minPlayers').textContent = game.minPlayers || 'N/A';
         document.getElementById('maxPlayers').textContent = game.maxPlayers || 'N/A';
-        document.getElementById('gameIMG').src = game.imageURL;
+        document.getElementById('timeAvailable').textContent = game.timeAvailable || 'N/A';
+        document.getElementById('difficultyText').textContent = game.difficulty || 'N/A';
+
+
+        // Add Image or Images to Cards
+        const toolsImagesContainer = document.getElementById('toolsImages');
+        toolsImagesContainer.innerHTML = '';
+
+        game.toolsRequired.forEach(toolImageUrl => {
+            const img = document.createElement('img');
+            img.src = toolImageUrl;
+            img.alt = 'Tool Image';
+            toolsImagesContainer.appendChild(img);
+        });
         // Update other fields similarly
     } else {
         console.log("Game not found");
     }
 }
+
+displayGameDetails(1);
