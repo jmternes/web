3e3f3138b5a66d8f601369bd0bd458afcd064e67
jmternes <68615887+jmternes@users.noreply.github.com>
2024-01-31 00:32:02 -0600

    data structuring, css, ux wording

--- bored-game/data.js
+++ bored-game/data.js
@@ -6,13 +6,13 @@ export const gamesData = [
       "minPlayers": 3,
       "maxPlayers": 8,
       "toolsRequired": ["./assets/images/cards.png", "./assets/images/coin.png"],
-      "toolsText": ["Cards", "Money"],
+      "toolsText": ["Cards", "Chips"],
       "imageURL": "",
       "timeAvailable": 10,
       "difficulty": "Easy",
       "drinking": "FALSE",
       "activity": "Relaxed",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": ""
     },
     {
@@ -28,9 +28,10 @@ export const gamesData = [
       "difficulty": "Moderate",
       "drinking": "FALSE",
       "activity": "Involved",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "High-Energy", "Playful", "Rule-Abiding", "Loud"],
       "rules": "Rules: Use a standard 52-card deck for 2 to 8 players. Deal 10 cards for 2 to 4 players, or 7 cards for 5 or more players. Place the remaining deck face down as a draw pile and turn the top card face up to start the discard pile. Gameplay: The objective is to score 500 points by forming sets and runs with your cards. Take turns drawing a card and optionally laying down sets/runs or adding to existing ones. End your turn by discarding a card onto the discard pile. Points are scored for valid sets/runs laid down on the table, while ungrouped cards count against your score. The first player to reach or exceed 500 points at the end of a hand wins."
     },
+
     {
       "GameID": 3,
       "name": "Blackjack",
@@ -44,7 +45,7 @@ export const gamesData = [
       "difficulty": "Easy",
       "drinking": "FALSE",
       "activity": "Involved",
-      "vibes": ["Chill", "Tight", "blackjack vibes", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": "Rules:<br> Blackjack is played with a standard deck of 52 cards. Players aim to create a hand value as close to 21 as possible without exceeding it. Deal 2 cards to each player, including the dealer. The dealer's first card is face-up, while the second is face-down.<br><br>\n\nGameplay:<br> Players take turns deciding whether to \"hit\" (take another card) or \"stand\" (keep their current hand). Face cards are worth 10 points, aces can be 1 or 11 points, and other cards are their face value. The player who gets closest to 21 without going over wins the round. If a player or the dealer goes over 21, they \"bust\" and lose the round. The first player to win a predetermined number of rounds or reach a target score wins the game."
     },
 
@@ -61,7 +62,7 @@ export const gamesData = [
       "difficulty": "Easy",
       "drinking": "FALSE",
       "activity": "Involved",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": ""
     },
 
@@ -78,7 +79,7 @@ export const gamesData = [
       "difficulty": "Hard",
       "drinking": "FALSE",
       "activity": "Involved",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": "<strong>Rules:</strong><br> Bridge is played with a standard deck of 52 cards and four players in two partnerships. Each player is dealt 13 cards. Players sit opposite their partners. The dealer shuffles and deals the cards clockwise.<br><br>\n\n<strong>Gameplay:</strong><br> The objective is to win tricks by playing higher-ranking cards in a series of deals. Players communicate through bids to determine the trump suit or whether to play in no-trump. The player to the dealer's left leads the first card, and players must follow suit if possible. The highest-ranking card of the led suit wins the trick. Points are scored for successfully making the contracted number of tricks based on bids. The partnership with the most points at the end of the game wins."
     },
 
@@ -95,7 +96,7 @@ export const gamesData = [
       "difficulty": "Easy",
       "drinking": "FALSE",
       "activity": "Involved",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": ""
     },
 
@@ -112,7 +113,7 @@ export const gamesData = [
       "difficulty": "Easy",
       "drinking": "FALSE",
       "activity": "Relaxed",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": ""
     },
 
@@ -122,11 +123,11 @@ export const gamesData = [
       "description": "Cribbage is a classic card game known for its unique  scoring system. Players score points by forming combinations of cards.   The game is traditionally played with a special Cribbage board used for   scorekeeping.",
       "minPlayers": 2,
       "maxPlayers": 4,
-      "toolsRequired": ["./assets/images/cards.png", "./assets/images/paper.png"],
-      "toolsText": ["Cards", "Cribbage Board"],
+      "toolsRequired": ["./assets/images/cards.png"],
+      "toolsText": ["Cards"],
       "timeAvailable": 30,
       "difficulty": "Moderate",
-      "vibes": ["Strategic", "Competitive", "Engaging"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
     },
 
     {
@@ -142,7 +143,7 @@ export const gamesData = [
       "difficulty": "Easy",
       "drinking": "TRUE",
       "activity": "Involved",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": ""
     },
 
@@ -159,7 +160,7 @@ export const gamesData = [
       "difficulty": "Moderate",
       "drinking": "FALSE",
       "activity": "Involved",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": "<strong>Rules: Hearts is played with a standard 52-card deck. Remove the 2 of clubs to start with 51 cards. Deal 13 cards to each player if you have 4 players. With 3 players, deal 17 cards to each player. If there are 5 players, use all 51 cards and deal 10 cards to each player.<br><br>\n\n<strong>Gameplay:</strong><br>The objective is to avoid taking cards with hearts and the queen of spades, which carry penalty points. In each trick, the player with the 2 of clubs leads. Other players must follow suit if possible. The player with the highest card of the led suit wins the trick. Hearts cannot be led until they have been \"broken\" by a player taking hearts in a previous trick. The game continues until all cards are played. The player with the fewest penalty points wins the game."
     },
 
@@ -176,7 +177,7 @@ export const gamesData = [
       "difficulty": "Easy",
       "drinking": "FALSE",
       "activity": "Relaxed",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": ""
     },
 
@@ -193,7 +194,7 @@ export const gamesData = [
       "difficulty": "Easy",
       "drinking": "FALSE",
       "activity": "Involved",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": ""
     },
 
@@ -209,7 +210,7 @@ export const gamesData = [
       "difficulty": "Easy",
       "drinking": "FALSE",
       "activity": "Relaxed",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": ""
     },
 
@@ -226,7 +227,7 @@ export const gamesData = [
       "difficulty": "Easy",
       "drinking": "FALSE",
       "activity": "Involved",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": ""
     },
 
@@ -243,7 +244,7 @@ export const gamesData = [
       "difficulty": "Easy",
       "drinking": "FALSE",
       "activity": "Relaxed",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": ""
     },
 
@@ -260,7 +261,7 @@ export const gamesData = [
       "difficulty": "Moderate",
       "drinking": "FALSE",
       "activity": "Competitive",
-      "vibes": ["Chill", "Tight", "Competitive", "Relaxed", "Children Friendly"],
+      "vibes": ["Competitive", "Chill", "Playful", "Free-Flowing", "Loud"],
       "rules": ""
     },
 
